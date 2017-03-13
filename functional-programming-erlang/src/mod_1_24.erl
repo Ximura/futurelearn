5f3c9b9c3ebc1561b0e0429f9e9c7a70e547476e
keyki <orukin@gmail.com>
2017-03-13 23:58:23 +0200

    fix incorect module name

--- functional-programming-erlang/src/mod_1_24.erl
+++ functional-programming-erlang/src/mod_1_24.erl
@@ -1,12 +1,12 @@
 %%%-------------------------------------------------------------------
 %%% @author Keyki
-%%% @copyright (C) 2017, <COMPANY>
+%%% @copyright (C) 2017
 %%% @doc
 %%%
 %%% @end
-%%% Created : 13. Март 2017 23:12
+%%% Created : 13 March 2017 23:12
 %%%-------------------------------------------------------------------
--module(mod_main).
+-module(mod_1_24).
 -author("Keyki").
 
 %% API
@@ -16,8 +16,3 @@ perimeter({circle, {R} }) -> 2 * math:pi() * R;
 perimeter({triangle, {A,B,C} }) -> A + B + C;
 perimeter({rectangle, {A, B}}) -> 2 * (A + B);
 perimeter({square, {A}}) -> perimeter({rectangle, {A, A}}).
-
-
-
-
-
