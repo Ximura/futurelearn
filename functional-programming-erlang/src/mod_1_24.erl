25ff754b19d890eeeded9892cd86fd2d1163126c
keyki <orukin@gmail.com>
2017-03-19 16:13:43 +0200

    Add rebar app

--- functional-programming-erlang/src/mod_1_24.erl
+++ functional-programming-erlang/src/mod_1_24.erl
@@ -10,7 +10,7 @@
 -author("Keyki").
 
 %% API
--export([perimeter/1, area/1]).
+-export([perimeter/1, area/1, enclose/1]).
 
 %%
 % Perimeter
@@ -28,6 +28,24 @@ area({circle, {R} }) -> math:pi() * R * R;
 area({rectangle, {A, B}}) -> A * B;
 area({square, {A}}) -> A * A;
 area({triangle, {A, B, C} }) ->
-  D = math:pow((B*B + C*C - A*A) / 2 * B, 2),
-  H = math:sqrt(C*C - D),
+  H = triangle_height(A, B, C),
   H * B / 2.
+
+%%
+% Enclose
+%%
+enclose({circle, {R} }) -> {rectangle, {2*R, 2*R}};
+enclose({rectangle, {A, B}}) -> {rectangle, {A, B}};
+enclose({square, {A}}) -> {rectangle, {A, A}};
+enclose({triangle, {A, B, C} }) ->
+  H = triangle_height(A, B, C),
+  {rectangle, {H, B}}.
+
+
+%%
+% Helpers
+%%
+
+triangle_height(A, B, C) ->
+  D = math:pow((B*B + C*C - A*A) / 2 * B, 2),
+  math:sqrt(C*C - D).
---
+++ functional-programming-erlang/tst/mod_1_24_test.erl
@@ -0,0 +1,20 @@
+%%%-------------------------------------------------------------------
+%%% @author Keyki
+%%% @copyright (C) 2017, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 19. Март 2017 14:21
+%%%-------------------------------------------------------------------
+-module(mod_1_24_test).
+-author("Keyki").
+
+-include_lib("eunit/include/eunit.hrl").
+
+%% API
+-export().
+
+simple_test() -> ?assert(1 + 1 =:= 2).
+
+%%  perimeter_test() -> mod_1_24:perimeter({circle, {10}})
+
