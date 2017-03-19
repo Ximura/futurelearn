1f8ccfa631abe24b0c5696adad6a310954ca5697
keyki <orukin@gmail.com>
2017-03-19 14:08:45 +0200

    add area

--- functional-programming-erlang/src/mod_1_24.erl
+++ functional-programming-erlang/src/mod_1_24.erl
@@ -10,9 +10,24 @@
 -author("Keyki").
 
 %% API
--export([perimeter/1]).
+-export([perimeter/1, area/1]).
 
+%%
+% Perimeter
+%%
 perimeter({circle, {R} }) -> 2 * math:pi() * R;
-perimeter({triangle, {A,B,C} }) -> A + B + C;
+perimeter({triangle, {A, B, C} }) -> A + B + C;
 perimeter({rectangle, {A, B}}) -> 2 * (A + B);
-perimeter({square, {A}}) -> perimeter({rectangle, {A, A}}).
+perimeter({square, {A}}) -> 4 * A.
+
+%%
+% Area
+%%
+
+area({circle, {R} }) -> math:pi() * R * R;
+area({rectangle, {A, B}}) -> A * B;
+area({square, {A}}) -> A * A;
+area({triangle, {A, B, C} }) ->
+  D = math:pow((B*B + C*C - A*A) / 2 * B, 2),
+  H = math:sqrt(C*C - D),
+  H * B / 2.
